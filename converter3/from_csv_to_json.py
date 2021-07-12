9189a34373bad66cafc8272a4206a5c56692c292
glad-spider <frstnm.lstnm.1444@mail.ru>
2021-07-12 12:08:39 +0300

    correct file

--- converter3/from_csv_to_json.py
+++ converter3/from_csv_to_json.py
@@ -1,58 +1,120 @@
 from converter_file_format import Converter
-from json import dumps
 
 class CSV_JSON(Converter):
 
+    def read_header(self):
+        pass
+
+    def record_value(self):
+        pass
+
     def __init__(self, file_csv):
         self.nmcsv = file_csv
-        self.data_csv = []          #храниние получаемой инфу из файла
+        self.data_csv = []          #храниние получаемой инфу(rows) из файла
         self.open()                 #прочитать файл
-        self.data_json = {}         #запись в формате словаря
-        self.header_keys = []
-        self.read_header()
+        self.data_cell, self.data_global = [], []
+        self.out_data_json = []
 
     def close(self):
         pass
 
+    #return rows of the csv file
     def open(self):
         with open(self.nmcsv, 'r') as fl:
             for line in fl:
                 self.data_csv.append(line.strip('\n'))
 
-        print(self.data_csv)
+    def common_action(self, line, pttrn):
+        last_poss = line.find(pttrn)
+        if last_poss == -1:
+            self.data_cell.append(line[0:])
+            line = None
+            return None
+        try:
+            if line[last_poss+1] == ",":
+                self.data_cell.append(line[0:last_poss+1])
+            else: self.data_cell.append(line[0:last_poss])
+        except IndexError as er:
+            pass
 
-    def read_header(self):
-        self.header_keys = self.data_csv[0].split(',')
+        line= line[last_poss + 1:]
+        return line
 
-    #index - poss cell(column)
-    def get_data_column(self, index):
-        some_dt = {}
-        for line in range(1, len(self.data_csv)):
-            some_dt[line] = self.data_csv[line].split(',')[index]
+    def seek_data_cell(self, line):
 
-        #print(some_dt)
-        return  some_dt
+        """
+            если строка(ячейка) начинается на " то етсь предложени, то мы ищем конец этой строки
+            то есть символы ",
+            """
+        """
+            если строка начинается с ' то есть цисло , то мы ищем просто ',
+        """
+        """
+            если это одно слово/символ
+        """
+        """
+            если первый сивол это пробел
+        """
+        #print(type(line), line)
+        if line == "" or line == None: return None
 
-    #key = self.header_keys[i]
-    def record_value(self, key, index):
-        self.data_json[f'{key}'] = self.get_data_column(index)
+        elif line[0] == '"':
+            #if str is some words ".... ....., ..."
+            line = self.common_action(line, '",')
 
-    def change_on(self):
-        for index in range(len(self.header_keys) - 1):
-            self.record_value(key=self.header_keys[index], index=index)
+        elif line[0] == "'":
+            line = self.common_action(line, "'")
+
+        elif line[0].isalpha() == True or line[0].isdigit() == True:
+            #if str without ',' in line
+            line = self.common_action(line, ',')
+
+        elif line[0] == ' ' or line[0] == ',':
+            line = line[1:]
+
+        else:
+            line = self.common_action(line,',')
+
+
+        self.seek_data_cell(line)
+
+
+    def get_header_cell(self, index):
+        return self.data_global[0][index]
+
+    def record_to_file(self):
+
+        for i in range(1, len(self.data_global) -1):
+
+            data_cell = dict()
+            for j in range(0,2):
+                data_cell[self.get_header_cell(index=j)] = self.data_global[i][j]
+            self.out_data_json.append(data_cell)
+
+        with open(f"{self.nmcsv.split('.')[0]}.json", "w") as _json:
+            _json.write(str(self.out_data_json))
+
+
+    def start(self):
+        rows = self.data_csv
+        for row in rows:
+            self.seek_data_cell(row)
+            self.data_global.append(self.data_cell)
+            #print(self.data_cell)
+            self.data_cell = []
 
         self.record_to_file()
 
-    def show_result(self):
-        self.change_on()
-
-    def record_to_file(self):
-        import json
-        with open('output.json', 'w') as fl:
-            json.dump(self.data_json, fl)
 
 
-c = CSV_JSON('addresses.csv')
-c.show_result()
+
+
+
+
+
+
+
+
+
 
 
